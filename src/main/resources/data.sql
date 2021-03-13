346dd9a19d1f51b1d96a088dbdcb19a4bc21b5f4
Muhamad Aboodia <muhamad.aboodia@outlook.com>
2021-03-13 18:50:32 +0000

    Update data to have some patients with no schedules.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,42 +1,31 @@
-INSERT INTO patient (patient_id, patient_name)
-VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '10' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Muhamad Aboodia');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '10' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '20' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Pei-Ying Chen');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '20' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '30' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Emma Jones');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '30' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '40' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Marco Reus');
+--INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '40' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '50' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Xin Wang');
+--INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '50' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '60' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Phil Kruse');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '60' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '70' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Stephen Edwards');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '70' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '80' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Dean Smith');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '80' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '90' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'El Nazeer Nazar');
+--INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '90' MINUTE, sysdate);
 
-INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Patient '||''||patient_id_seq.currval||'');
-INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on)
-VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '100' MINUTE, sysdate);
+INSERT INTO patient (patient_id, patient_name) VALUES (patient_id_seq.nextval, 'Chandana Devireddy');
+INSERT INTO schedule (schedule_id, patient_id, start_date, requested_on) VALUES (schedule_id_seq.nextval, patient_id_seq.currval, sysdate + INTERVAL '100' MINUTE, sysdate);
 
 COMMIT;
